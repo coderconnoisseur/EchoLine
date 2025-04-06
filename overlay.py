cb4ab8182ba151a386a1f8babf9f41a3f5791dd2
coderconnoisseur <142653086+coderconnoisseur@users.noreply.github.com>
2025-04-06 12:36:29 +0530

    white bg added

--- overlay.py
+++ overlay.py
@@ -5,10 +5,11 @@ class Overlay:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Live Captions")
-        self.root.attributes('-topmost', True) 
+        self.root.attributes('-topmost', True)
+        self.root.attributes('-alpha',0.8) 
         self.root.geometry("800x100+100+100") 
         self.root.overrideredirect(True) 
-        self.root.wm_attributes("-transparentcolor", "white") 
+        self.root.configure(background='black')
         self.subtitle_label = tk.Label(self.root, text="", font=("Arial", 24), bg="white", fg="black")
         self.subtitle_label.pack(expand=True, fill='both')
     def update_subtitle(self,text):
